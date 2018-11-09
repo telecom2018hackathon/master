c6e2fd2da06e7b3ecabb1a1fb58fd274946078ae
Ly Adrien <lyadrien@Lys-MacBook-Pro.local>
2018-11-10 00:10:50 +0100

    setting of the general path for the true_labels_trainning.txt file

--- label/label_to_df_and_count.py
+++ label/label_to_df_and_count.py
@@ -3,6 +3,7 @@ import numpy as np
 
 # ouverture du fichier true_label
 path = '*/true_labels_training.txt'
-f = open(path)
+path2 = 'true_labels_training.txt'
+f = open(path2)
 labels = f.read()
 ds_label = pd.Series(tuple(labels))
