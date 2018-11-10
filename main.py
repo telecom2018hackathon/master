9a7b2cc20cab4ce2bbc06eab68ce2f75bd75b17b
Default user for cloud <cloud@ecs-2cd1-0001.novalocal>
2018-11-10 01:20:09 +0000

    Ajout code TOM sur les labels training

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import glob
 import re
 import os
@@ -8,7 +10,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-path = "~/hackathon/training_dataset/"
+# ------------------------------------------------
+# -                   TOM                        -
+# ------------------------------------------------
+
+
+# ouverture du fichier true_label
+path_true_labels_training = '/home/cloud/hackathon/true_labels_training.txt'
+
+# Converts training label file to dataframe for easy retrieval
+# Returns: dataframe 1col int (0 or 1)
+def label_to_data_series():
+    f = open(path_true_labels_training)
+    labels = f.read()
+    return pd.Series(tuple(labels))
+
+
+
+path_training_dataset = '/home/cloud/hackathon/training_dataset'
 
 # ------------------------------------------------
 # -                   JACKY                      -
@@ -17,7 +36,7 @@ path = "~/hackathon/training_dataset/"
 ## Récupération de la liste de fichiers en fonction de l'extension
 
 
-def training_files_to_list(p):
+def training_files_to_list(path):
     """Create a dict with paths and key in key. 
     
     Variables :
@@ -26,26 +45,26 @@ def training_files_to_list(p):
     Return :
         L = [(XXXXXX,process_path,behavior_path)]   
         """
-    os.chdir(p)
-    
-    filenames_proc = glob.glob('*_process_generation.txt')
+
+    filenames_proc = glob.glob(f'{path}/*_process_generation.txt')
 
     re_index = r"(\d+)"
     
     return {re.findall(re_index,elmt)[0]:
-                         [path + "/" + elmt,
-                         path + "/" + re.sub('process_generation','behavior_sequence', elmt)] 
+                         [elmt,
+                         re.sub('process_generation','behavior_sequence', elmt)] 
                          for elmt in filenames_proc}
     
+dico = training_files_to_list(path_training_dataset)
 
 def path_from_index(index,file):
     
     """return the path given index number and file type (process or behavior)
     
     index : str
-    file : string : behavior or process"""
+    file : string : behavior or process
+    dico : dictionnaire avec indexes """
     
-    dico = training_files_to_list(path)
     
     if file == "behavior":
         return dico[str(index)][1]
@@ -60,9 +79,6 @@ def path_from_index(index,file):
 # ------------------------------------------------
 # -                   ARNAUD                     -
 # ------------------------------------------------
-
-
-file_name = '/training_108006_behavior_sequence.txt'
 
 def behavior_file_to_pandas(index):
 	filepath = path_from_index(index, 'behavior')
