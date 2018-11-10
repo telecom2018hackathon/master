16cc4efd8310a7724e5f51d98135eca12e358045
Default user for cloud <cloud@ecs-2cd1-0001.novalocal>
2018-11-10 00:55:36 +0000

    behavior_file_to_metrics

--- main.py
+++ main.py
@@ -1,21 +1,75 @@
+import glob
+import re
+import os
 import sys
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-csv.field_size_limit(sys.maxsize)
 
-listofstr = []
-"""
-with open('../true_labels_training.txt') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		listofstr.append(row)
+path = "~/hackathon/training_dataset/"
 
-print(listofstr)
-"""
+# ------------------------------------------------
+# -                   JACKY                      -
+# ------------------------------------------------
 
-df = pd.DataFrame(np.ones(50))
-df.plot()
-plt.show()
+## Récupération de la liste de fichiers en fonction de l'extension
+
+
+def training_files_to_list(p):
+    """Create a dict with paths and key in key. 
+    
+    Variables :
+        Path : location of the files
+    
+    Return :
+        L = [(XXXXXX,process_path,behavior_path)]   
+        """
+    os.chdir(p)
+    
+    filenames_proc = glob.glob('*_process_generation.txt')
+
+    re_index = r"(\d+)"
+    
+    return {re.findall(re_index,elmt)[0]:
+                         [path + "/" + elmt,
+                         path + "/" + re.sub('process_generation','behavior_sequence', elmt)] 
+                         for elmt in filenames_proc}
+    
+
+def path_from_index(index,file):
+    
+    """return the path given index number and file type (process or behavior)
+    
+    index : str
+    file : string : behavior or process"""
+    
+    dico = training_files_to_list(path)
+    
+    if file == "behavior":
+        return dico[str(index)][1]
+    
+    if file == "process":
+        return dico[str(index)][0]
+    
+    else:
+        return None
+
+
+# ------------------------------------------------
+# -                   ARNAUD                     -
+# ------------------------------------------------
+
+
+file_name = '/training_108006_behavior_sequence.txt'
+
+def behavior_file_to_pandas(index):
+	filepath = path_from_index(index, 'behavior')
+	data = pd.read_csv(filepath, header = None)
+	data.columns = columns = ['PROCESS', 'RPI', 'API']
+	return data
+
+def behavior_file_to_metrics(index):
+	data = behavior_file_to_pandas(index)
+	return data.describe().loc[['count', 'unique', 'freq'],:]
