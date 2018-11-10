dc73a327df04bcc3d3af2838c2d85a455674020f
Default user for cloud <cloud@ecs-2cd1-0001.novalocal>
2018-11-10 05:56:47 +0000

    Premier fichier XY

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import time 
+from multiprocessing import Process
 
 
 # ------------------------------------------------
@@ -23,8 +25,7 @@ path_true_labels_training = '/home/cloud/hackathon/true_labels_training.txt'
 def label_to_data_series():
     f = open(path_true_labels_training)
     labels = f.read()
-    return pd.Series(tuple(labels))
-
+    return pd.DataFrame(np.array([tuple(labels)]).T, columns = ['LABELS'])
 
 
 path_training_dataset = '/home/cloud/hackathon/training_dataset'
@@ -36,6 +37,13 @@ path_training_dataset = '/home/cloud/hackathon/training_dataset'
 ## Récupération de la liste de fichiers en fonction de l'extension
 
 
+def index_to_path(index, typ):
+    if typ == 'behavior' :
+        return path_training_dataset + '/training_' + index + '_' + typ + '_sequence.txt'
+    elif typ == 'process' :
+        return path_training_dataset + '/training_' + index + '_' + typ + '_generation.txt'
+
+
 def training_files_to_list(path):
     """Create a dict with paths and key in key. 
     
@@ -45,47 +53,104 @@ def training_files_to_list(path):
     Return :
         L = [(XXXXXX,process_path,behavior_path)]   
         """
-
-    filenames_proc = glob.glob(f'{path}/*_process_generation.txt')
-
     re_index = r"(\d+)"
+        
+    filenames_proc = glob.glob(f'{path}/*_process_generation.txt')
+    f = [[re.findall(re_index,elmt)[0],
+         re.sub('process_generation','behavior_sequence', elmt),
+         elmt ]
+         for elmt in filenames_proc]
     
-    return {re.findall(re_index,elmt)[0]:
-                         [elmt,
-                         re.sub('process_generation','behavior_sequence', elmt)] 
-                         for elmt in filenames_proc}
-    
-dico = training_files_to_list(path_training_dataset)
+    return f
 
-def path_from_index(index,file):
     
-    """return the path given index number and file type (process or behavior)
+def to_pandas(path_training_dataset):
+    list_path = training_files_to_list(path_training_dataset)
+    t= time.time()
+    """return a dataset of the files in the dico"""
+    l = [[elmt[0],file_read(elmt[1]).split("\n"),file_read(elmt[2]).split("\n")] for elmt in list_path[n*i:n*(i+1)]][:1000]
     
-    index : str
-    file : string : behavior or process
-    dico : dictionnaire avec indexes """
+    print(t-time.time())
+    df = pd.DataFrame(l,columns = ["index","behavior","process"])
+    df = df.set_index("index")
+    return df
     
-    
-    if file == "behavior":
-        return dico[str(index)][1]
-    
-    if file == "process":
-        return dico[str(index)][0]
-    
-    else:
-        return None
-
-
+ 
+def file_read(file_path):
+    """return the string of the full content of a text"""
+    with open(file_path) as file:
+        data = file.read()
+    return data
 # ------------------------------------------------
 # -                   ARNAUD                     -
 # ------------------------------------------------
 
 def behavior_file_to_pandas(index):
-	filepath = path_from_index(index, 'behavior')
-	data = pd.read_csv(filepath, header = None)
-	data.columns = columns = ['PROCESS', 'RPI', 'API']
-	return data
+    filepath = index_to_path(index, 'behavior')
+    data = pd.read_csv(filepath, header = None)
+    data.columns = columns = ['PROCESS', 'RPI', 'API']
+    return data
 
 def behavior_file_to_metrics(index):
-	data = behavior_file_to_pandas(index)
-	return data.describe().loc[['count', 'unique', 'freq'],:]
+    data = behavior_file_to_pandas(index)
+    return data.describe().loc[['count', 'unique', 'freq'],:]
+
+def behavior_metrics(index):
+    data = behavior_file_to_metrics(index)
+    return [data.loc['count','PROCESS'], data.loc['unique', 'RPI'], data.loc['unique', 'API']]
+
+
+# ------------------------------------------------
+# -                   MAT                        -
+# ------------------------------------------------
+# List of metrics regarding the processes
+def process_metrics(index):
+    filepath = index_to_path(index, 'process')
+    df = pd.read_csv(filepath, header=None, sep=' -> ', engine='python')
+    return [len(df), len(np.unique(df.values))]
+
+
+
+
+
+# X creation
+def thread_run(start, stop, threadNumber):
+    l = []
+    errorCount = 0
+    for i in range(start, stop) :
+        if i%1000 == 0:
+            print(str(threadNumber) + " : " + str(i))
+        index = str(i).zfill(6)
+        try :
+            l.append([index] + behavior_metrics(index) + process_metrics(index))
+        except :
+            errorCount += 1
+    pd.DataFrame(l).to_csv('./X_'+ str(threadNumber) + '_V2.csv')
+
+
+def launch_processes() :
+    for i in range(40):
+        p = Process(target = thread_run, args = (i*5000, (i+1)*5000, i))
+        p.start()
+
+def concat_csv():
+    df = pd.DataFrame()
+    for i in range(40) :
+        temp = pd.read_csv('./X_' + str(i) + '_V2.csv', header = 0, index_col = 0)
+        temp.reindex(temp.index.values + i*5000)
+        df = pd.concat([df,temp], ignore_index = True)
+    df.columns = ['Index', 'Behavior_lenght', 'Behavior_dist_RPI', 'Behavior_dist_API', 'Process_length', 'Process_dist' ]
+    df.set_index('Index', inplace=True)
+    df.to_csv('./XFinal_V1.csv')
+
+def labelisation():
+    labels = label_to_data_series()
+    data = pd.read_csv('./XFinal_V1.csv', index_col = 0, header = 0)
+    data = pd.merge(data, labels, left_index=True, right_index=True)
+    data.to_csv('./XY.csv')
+
+labelisation()
+
+
+
+
