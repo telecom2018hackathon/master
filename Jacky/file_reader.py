aa44d81ad483ca79801195195e09fa332f8b7228
jakaub <jacky.kaub@telecom-paristech.fr>
2018-11-10 03:17:34 +0100

    debbug

--- Jacky/file_reader.py
+++ Jacky/file_reader.py
@@ -4,6 +4,8 @@ path = "/home/jacky/test"
 import glob
 import re
 import os
+import pandas as pd
+import time 
 
 def training_files_to_list(path):
     """Create a dict with paths and key in key. 
@@ -14,34 +16,34 @@ def training_files_to_list(path):
     Return :
         L = [(XXXXXX,process_path,behavior_path)]   
         """
-
-    filenames_proc = glob.glob(f'{path}/*_process_generation.txt')
-
     re_index = r"(\d+)"
+        
+    filenames_proc = glob.glob(f'{path}/*_process_generation.txt')
+    f = [[re.findall(re_index,elmt)[0],
+         re.sub('process_generation','behavior_sequence', elmt),
+         elmt ]
+         for elmt in filenames_proc]
     
-    return {re.findall(re_index,elmt)[0]:
-                         [elmt,
-                         re.sub('process_generation','behavior_sequence', elmt)] 
-                         for elmt in filenames_proc}
+    return f
     
 
 dico = training_files_to_list(path)
-def path_from_index(index,file):
     
-    """return the path given index number and file type (process or behavior)
+def to_pandas(list_path):
+    t= time.time()
+    """return a dataset of the files in the dico"""
+    l = [[elmt[0],file_read(elmt[1]).split("\n"),file_read(elmt[2]).split("\n")] for elmt in list_path]
     
-    index : str
-    file : string : behavior or process
-    dico : dictionnaire avec indexes """
+    print(t-time.time())
+    df = pd.DataFrame(l,columns = ["index","behavior","process"])
+    df = df.set_index("index")
+    return df
     
-    
-    if file == "behavior":
-        return dico[str(index)][1]
-    
-    if file == "process":
-        return dico[str(index)][0]
-    
-    else:
-        return None
-    
+ 
+def file_read(file_path):
+    """return the string of the full content of a text"""
+    with open(file_path) as file:
+        data = file.read()
+    return data
+        
     
